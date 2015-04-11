12f46a9b884845e9f11076ab91a6b66597c7b77d
chendind <364044816@qq.com>
2015-04-11 13:01:54 +0800

    public file

--- Public/js/userinfo.js
+++ Public/js/userinfo.js
@@ -30,4 +30,53 @@ userinfo.config(['$routeProvider', function($routeProvider) {
         });
     }]).controller('UserinfoCtrl',["$scope",function($scope){
     }])
+    .
+                directive('draggable', function() {
+                    
+                        return function(scope, element) {
+                             var startX = 0,
+                                             startY = 0,
+                                             x =0,
+                                             y = 0;
+                                    element.bind('mousedown', function(evt) {
+                                            x =element.position().left;
+                                             y = element.position().top;
+                                            startX = evt.screenX - x;
+                                            startY = evt.screenY - y;
+
+                                            element.bind('mousemove', mousemove);
+                                            element.bind('mouseup', mouseup);
+                                    });
+
+                                    function mousemove(evt) {
+                                            y = evt.screenY - startY;
+                                            x = evt.screenX - startX;
+                                            element.css({
+                                                    top: y + "px",
+                                                    left: x + "px"
+                                            })
+                                    }
+
+                                    function mouseup() {
+                                            element.unbind('mousemove', mousemove);
+                                            element.unbind('mouseup', mouseup);
+                                    }
+                                 }
+                })
+   .directive('dragbar',function(){
+        return function(scope, element){
+            element.bind('mousedown',function(evt){
+                x=element.position().left;
+                y=element.position().top;
+                startX = evt.screenX - x;
+                startY = evt.screenY - y;
+
+                element.bind('mousemove', mousemove);
+                element.bind('mouseup', mouseup);
+            });
+            function mousemove(evt){
+                y=evt.screenY - startY;
+            }
+        }
+   })
 
